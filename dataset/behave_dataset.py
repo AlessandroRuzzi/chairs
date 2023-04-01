cbfcb8b7cde49652f7b1eb6c9cc078fe1d7f9afa
AlessandroRuzzi <alrxruzzi98@gmail.com>
2023-04-01 17:14:06 +0200

    u

--- dataset/behave_dataset.py
+++ dataset/behave_dataset.py
@@ -185,7 +185,7 @@ class BehaveImgDataset(BaseDataset):
                             if np.sum(mask) / np.sum(full_mask) < 0.3:
                                 continue
                         print(category)
-                        if exists(img_path) and exists(mask_path) and exists(pvqout_path) and exists(obj_path) and category == 'chairblack':
+                        if exists(img_path) and exists(mask_path) and exists(pvqout_path) and exists(obj_path) and category == 'backpack':
                             self.data.append({
                                 'img_path': img_path,
                                 'mask_path': mask_path,
