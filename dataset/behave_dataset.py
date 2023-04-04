4a9d3314df1024e264b68470a7c69dec06b39eb8
AlessandroRuzzi <alrxruzzi98@gmail.com>
2023-04-04 22:54:37 +0200

    u

--- dataset/behave_dataset.py
+++ dataset/behave_dataset.py
@@ -312,7 +312,7 @@ class BehaveImgDataset(BaseDataset):
         human_betas = self.pare['betas']
         human_orient = transforms.matrix_to_axis_angle(self.pare['global_orient'])
         human_transl = self.pare['transl']
-        print('------------------> ', self.pare['expression'].shape)
+        print('------------------> ', human_orient.shape)
         ret['human_pose'] = human_pose
         ret['human_betas'] = human_betas
         ret['human_orient'] = human_orient
